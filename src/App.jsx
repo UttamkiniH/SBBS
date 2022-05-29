5bba90c3afeac3e9342e713608ba0793335e28ff
vital <66871109+vital345@users.noreply.github.com>
2022-05-29 19:53:55 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Root from "./components/root/Root";
+import AddUser from "./components/add-user/AddUser";
+import Admin from "./components/admin/Admin";
+import Login from "./components/login/Login";
+import AddTransaction from "./components/add-transactions/AddTransaction";
+import CreateRole from "./components/create-role/CreateRole";
+import MyTransactions from "./components/myTransactions/MyTransactions";
+
+import {useAuthCurr} from "./firebase-config"
+
+function App() {
+	const currUser = useAuthCurr();
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Root />} />
+					<Route exact path="/add-user" element={<AddUser />} />
+					<Route exact path="/admin" element={<Admin />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/add-transaction" element={<AddTransaction currUser={currUser}/>} />
+					<Route exact path="/create-role" element={<CreateRole />} />
+					<Route exact path="/my-transaction" element={<MyTransactions />} />
+				</Routes>
+			</Router>
+		</>
+	);
+}
+
+export default App;
